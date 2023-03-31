c12b41000698194c196f1424b4670ca10c50ed40
caolp <caolp@startimes.com.cn>
2023-03-31 17:04:59 +0800

    使用 RequsetMapping 书写

--- service/open-ai/src/main/java/com/star/openai/api/chat/ChatCompletionServiceImpl.java
+++ service/open-ai/src/main/java/com/star/openai/api/chat/ChatCompletionServiceImpl.java
@@ -4,12 +4,14 @@ import com.plexpt.chatgpt.ChatGPTStream;
 import com.plexpt.chatgpt.entity.chat.ChatCompletion;
 import com.plexpt.chatgpt.entity.chat.Message;
 import com.plexpt.chatgpt.listener.SseStreamListener;
+import com.plexpt.chatgpt.util.Proxys;
 import com.star.openai.OpenAiConfig;
 import com.star.openai.domain.chat.ChatRequest;
 import com.star.openai.domain.chat.ChatResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.net.Proxy;
 import java.util.List;
 
 /**
@@ -20,10 +22,11 @@ public class ChatCompletionServiceImpl implements IChatCompletionService {
 
     @Override
     public ChatResult<String> chatCompletion(ChatRequest request) {
+        Proxy proxy = Proxys.http("127.0.0.1", 1080);
         ChatGPTStream chatGptStream = ChatGPTStream.builder()
                 .timeout(600)
                 .apiKey(OpenAiConfig.getInstance().getOpenAiKey())
-                //.proxy(proxy)
+                .proxy(proxy)
                 .apiHost("https://api.openai.com/")
                 .build()
                 .init();
--- service/open-ai/src/main/java/com/star/openai/api/chat/IChatCompletionService.java
+++ service/open-ai/src/main/java/com/star/openai/api/chat/IChatCompletionService.java
@@ -3,6 +3,7 @@ package com.star.openai.api.chat;
 import com.star.openai.domain.chat.ChatRequest;
 import com.star.openai.domain.chat.ChatResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,5 +22,5 @@ public interface IChatCompletionService {
      */
     @PostMapping("/chat")
     @ResponseBody
-    ChatResult<String> chatCompletion(ChatRequest request);
+    ChatResult<String> chatCompletion(@RequestBody ChatRequest request);
 }
