60788838789dd974e49ff82e179d345d8e557fe8
caolp <caolp@startimes.com.cn>
2023-05-30 16:22:02 +0800

    使用 RequsetMapping 书写 customer-service

--- service/customer-service/src/main/java/com/star/customer/api/ICustomerService.java
+++ service/customer-service/src/main/java/com/star/customer/api/ICustomerService.java
@@ -4,13 +4,17 @@ import com.star.customer.domain.Customer;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("customer/")
-@RestController
 public interface ICustomerService {
 
     @RequestMapping(value = "save", method = RequestMethod.POST)
     @ResponseBody
     Customer save(Customer customer);
 
+    /**
+     * GET http://localhost:9005/customer/find-by-id?id=1
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "find-by-id", method = RequestMethod.GET)
     @ResponseBody
     Customer findById(@RequestParam("id") Long id);
