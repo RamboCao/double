394cf0979dd493c3d06e5283d6fbd94cf8231dd6
caolp <caolp@startimes.com.cn>
2023-11-13 10:50:17 +0800

    Repository 事务的详细测试，获取数据库连接，异常回滚等情况调试代码

--- service/order-service/src/main/java/com/star/order/api/OrdersService.java
+++ service/order-service/src/main/java/com/star/order/api/OrdersService.java
@@ -4,7 +4,9 @@ import com.star.order.dao.OrdersRepository;
 import com.star.order.domain.Orders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 
 /**
@@ -24,6 +26,26 @@ public class OrdersService implements IOrdersService {
     @Override
     public Orders findById(Long id) {
         return ordersRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * 这个方法主要用来测试 Repository 的事务，测试 JDBC connection 获取过程
+     * 以及 findById() 和 save() 事务之间的管理，独立事务，互不影响，抛出异常的情况：抛出异常也不会回滚
+     * 添加 @Transactional 注解之后事务的变化：变成同一个事务，同一个连接，抛出异常会进行回滚
+     * @EnableJpaRepositories(enableDefaultTransactions = false) 注解添加对于事务的影响，没有事务了，会报错，必须
+     * 添加 @Transactional 注解才能执行
+     * @param id
+     * @return
+     */
+    // @Transactional
+    public Orders serviceMethodTransactionTest(Long id){
+        Orders orders = ordersRepository.findById(id).orElse(null);
+        orders.setCode("3456789");
+        Orders orders1 = ordersRepository.save(orders);
+        orders1.setModifyInstant(Instant.now());
+        Orders orders2 = ordersRepository.saveAndFlush(orders1);
+        int i = 1/0;
+        return orders2;
     }
 
     @Override
